586629ebf386db6fe66d0a3cd08a0394bdd70b12
baarath <barath9620@gmail.com>
2017-07-20 09:33:33 -0700

    Add files via upload

---
+++ pro1.py
@@ -0,0 +1,7 @@
+baarath=('baarath','starlord')
+usename=input("enter the username...")
+password=input("enter the password\n")
+welcome="welcome mr. %s your password is %s"
+if password in baarath:
+	print (welcome % baarath)
+	
